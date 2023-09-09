6788f137557321bc7fe71f39f887e9166f334fe0
billal <boudjellbillal@gmail.com>
2023-09-09 14:50:20 +0200

    upd

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports = async() =>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/url')
+        await mongoose.connect('mongodb+srv://billal:sTVW7vpckkVHuHFV@cluster0.x9zv7hr.mongodb.net/URL?retryWrites=true&w=majority')
         console.log('database connection');
     } catch (error) {
         console.log(error);
